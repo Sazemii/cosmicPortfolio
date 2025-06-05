da761cc6f235aa50bd7ba413208f7442e7c4f2ff
ThunderRabbit <zuzazibubabi@gmail.com>
2025-06-05 18:49:57 +0800

    added a header, going to add animation for it now using framer

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      lol
-    </div>
+    <main className="">
+      <div className="Header flex justify-center gap-[4rem]">
+        <div className="homeBar nav">Home</div>
+        <div className="aboutBar nav">About</div>
+        <div className="contactBar nav">Contact</div>
+      </div>
+    </main>
   );
 }
